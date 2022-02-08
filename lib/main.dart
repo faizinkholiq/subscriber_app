b39c3c37620912521f353582108c1e169193c249
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-02-08 09:40:43 +0700

    refactor some code

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,47 @@ class PaymentApps extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xff04112F),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 50.0, left: 32, right: 32),
-            child: Column(
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/header.png',
-                    width: 230,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0, left: 75, right: 0),
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Text("Upgrade to", style: titleTextStyle,),
-                        SizedBox(width: 6,),
-                        Text("Pro", style: titleProTextStyle)
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top:16.0),
-                  child: Text("Go unlock all features and \nbuild your own business bigger", style: subtitleTextStyle, textAlign: TextAlign.center,),
-                )
-              ]
-            ),
-          ),
+          child: Header(),
         )
       )
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50.0, left: 32, right: 32),
+      child: Column(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/header.png',
+              width: 230,
+            ),
+          ),
+          SizedBox(height: 40,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Upgrade to", style: titleTextStyle,),
+              SizedBox(width: 6,),
+              Text("Pro", style: titleProTextStyle)
+            ],
+          ),
+          SizedBox(height: 16,),
+          Text(
+            "Go unlock all features and \nbuild your own business bigger", 
+            style: subtitleTextStyle, 
+            textAlign: TextAlign.center,
+          ),
+        ]
+      ),
     );
   }
 }
