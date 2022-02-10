3d11b122854ab81f0949448a6a373c8f4e8b30d3
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2022-02-10 07:48:56 +0700

    update state

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,24 @@ import 'package:uangkuu/theme.dart';
 
 void main() => runApp(PaymentApps());
 
-
-class PaymentApps extends StatefulWidget {
-  @override
-  State<PaymentApps> createState() => _PaymentAppsState();
-}
-
-class _PaymentAppsState extends State<PaymentApps> {
+class PaymentApps extends StatelessWidget {
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xff04112F),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Header(),
-              Option(),
-              Option(),
-              Option(),
-            ],
-          ),
-        )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Color(0xff04112F),
+            body: SafeArea(
+              child: Column(
+                children: [
+                  Header(),
+                  Option(0),
+                  Option(1),
+                  Option(2),
+                ],
+              ),
+            )));
   }
 }
 
@@ -40,48 +33,70 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 50.0, left: 32, right: 32),
-      child: Column(
-        children: [
-          Center(
-            child: Image.asset(
-              'assets/header.png',
-              width: 230,
+      child: Column(children: [
+        Center(
+          child: Image.asset(
+            'assets/header.png',
+            width: 230,
+          ),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Upgrade to",
+              style: titleTextStyle,
             ),
-          ),
-          SizedBox(height: 40,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Upgrade to", style: titleTextStyle,),
-              SizedBox(width: 6,),
-              Text("Pro", style: titleProTextStyle)
-            ],
-          ),
-          SizedBox(height: 16,),
-          Text(
-            "Go unlock all features and \nbuild your own business bigger", 
-            style: subtitleTextStyle, 
-            textAlign: TextAlign.center,
-          ),
-        ]
-      ),
+            SizedBox(
+              width: 6,
+            ),
+            Text("Pro", style: titleProTextStyle)
+          ],
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          "Go unlock all features and \nbuild your own business bigger",
+          style: subtitleTextStyle,
+          textAlign: TextAlign.center,
+        ),
+      ]),
     );
   }
 }
 
-class Option extends StatelessWidget {
-  const Option({ Key? key }) : super(key: key);
+class Option extends StatefulWidget {
+  final int index;
 
+  Option(this.index);
+
+  @override
+  State<Option> createState() => _OptionState();
+}
+
+class _OptionState extends State<Option> {
+  int selectedIndex = -1;
+  
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = widget.index;
+        });
+      },
       child: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.all(20),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.white),
-          borderRadius: BorderRadius.circular(14)
-        ),
+            border: Border.all(
+                color:
+                    selectedIndex == widget.index ? Color(0xff007DFF) : Color(0xff4D5B7C)),
+            borderRadius: BorderRadius.circular(14)),
       ),
     );
   }
